7b6c62bf15bc0a559bfabdfae608656d004a5f2a
Stepoquer <78288188+Stepoquer@users.noreply.github.com>
2021-02-03 08:23:55 -0300

    Create bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ dispatcher = updater.dispatcher
 lista_fotos=[]
 lista_stickers=[]
 lista_gifs=[]
-lista_chistes=["Cuando la vida te de la espalda, tócale el culo", "Iban 2 y se cayó el del medio",\
+lista_chistes=["Cuando la vida te de la espalda, tócale el culo", "-Tú sabés por qué el cincuenta y nueve está de pie?\n-No, por qué?\-Porque luego sesenta",\
 	       "¿Cuál es el colmo de los colmos? Que un mudo le diga a un sordo que un ciego lo está mirando", "-Buen día, quisiera alquilar Shrek Forever\n-Lo siento pero la tiene que devolver tomorrow",\
-	      "—Hola, soy paraguayo y quiero pedirle la mano de su hija para cogérmela\n—¿¡Para qué?!?\n—Paraguayo.", "Un hombre entra en un bar furioso y con una pistola en la mano y dice:\n-¿QUIÉN MIERDA SE\
-	      ACOSTÓ CON MI ESPOSA?\nA lo que un hombre al fondo del bar contesta:\n-Amigo, no tienes suficientes balas..."]
+	      "—Hola, soy paraguayo y quiero pedirle la mano de su hija para cogérmela\n—¿¡Para qué?!?\n—Paraguayo.", "Un hombre entra en un bar furioso y con una pistola en la mano y dice:\n-¿QUIÉN MIERDA SE ACOSTÓ CON MI ESPOSA?\nA lo que un hombre al fondo del bar contesta:\n-Amigo, no tienes suficientes balas...",\
+	      "Érase una vez un perro que respiraba por el culo, se sentó y se murió", ""]
 lista_amor=["Podré no verte, podré no hablarte, pero lo que jamás podré será olvidarte", "Tal vez no te de lo mejor del mundo, pero siempre tendrás lo mejor de mí"]
 
 tupla_todo=(lista_amor,lista_chistes,lista_fotos)
